f1e7e8cefc5f7b3720839d28546c404d47efd632
TechNas12 <dahotresanket12@gmail.com>
2026-03-13 23:32:10 +0530

    Added the data transformation pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
 
+from src.components.data_transform import DataTransformationConfig, DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path : str=os.path.join('artifact','train.csv')
@@ -43,5 +45,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformer = DataTransformation()
+    data_transformer.initiate_data_transform(train_data,test_data)
+
     
